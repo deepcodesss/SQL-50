6558b9e6a3e342c85ac5bc37e632f01f57bf806d
mrajdeep952 <mrajdeep952@gmail.com>
2025-05-03 20:14:41 +0530

    sql sol added

--- sql-176-leetcode.sql
+++ sql-176-leetcode.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+-- Write your MySQL query statement below
 
 select max(salary) as secondHighestSalary from employee 
 where salary < all(select distinct max(salary) from employee);
